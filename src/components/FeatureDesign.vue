bf1844de7f19531b651aa42bc09a553124d10455
aungminnkhant94 <aungminnkhant94@gmail.com>
2022-03-09 01:00:16 +0630

    features photo

--- src/components/FeatureDesign.vue
+++ src/components/FeatureDesign.vue
@@ -1,27 +1,43 @@
 <template>
     <section class="section-features mt-4">
-        <div class="u-center-text">
+        <div class="u-center-text mt-3">
             <h2 class="heading-secondary">
-                Featured Designs
+                Featured Collections
             </h2>
         </div>
+        <div class="row mt-3">
+          <div class="col-1-of-4">
+            <div class="card">
+              <div class="card__picture">
+              </div>
+              <h4 class="card__heading">
+                Custom Tailored
+                <br>
+                <span class="card_heading-span mx-auto">
+                  Suits
+                </span>
+              </h4>
+            </div>
+          </div>
 
-        <div class="container">
-          <div class="row">
-            <div class="col">
-              Column
+          <div class="col-1-of-4">
+            <div class="card">
+              <div class="card__picture">
+              </div>
             </div>
-            <div class="col">
-              Column
+          </div>
+
+          <div class="col-1-of-4">
+            <div class="card">
+              <div class="card__picture">
+              </div>
             </div>
-            <div class="col">
-              Column
-            </div>
-            <div class="col">
-              Column
-            </div>
-            <div class="col">
-              Column
+          </div>
+
+          <div class="col-1-of-4">
+            <div class="card">
+              <div class="card__picture">
+              </div>
             </div>
           </div>
         </div>
