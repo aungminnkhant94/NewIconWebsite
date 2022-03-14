52008ae573d41c6e01df2af149623056ffaa0c41
aungminnkhant94 <aungminnkhant94@gmail.com>
2022-03-14 23:20:52 +0630

    services

--- src/components/ServiceSection.vue
+++ src/components/ServiceSection.vue
@@ -8,7 +8,41 @@
         <div class="row">
             <div class="col-1-of-4">
                 <div class="services-box">
-                    <i class="services-box__icon"></i>
+                    <i class="services-box__icon fas fa-user-tie"></i>
+                    <h3 class="heading-tertiary mb-4">Premium Luxury Fabrics</h3>
+                    <p class="services-box__text">
+                        <b>Custom Suits</b> and Clothing from world renowned fabrics-Dormeuil. Loro piana, Ermenegildo Zegna, Holland & Sherry & More
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-1-of-4">
+                <div class="services-box">
+                    <i class="services-box__icon fal fa-ruler"></i>
+                    <h3 class="heading-tertiary mb-4">Craftsmanship</h3>
+                    <p class="services-box__text">
+                        More than 45 years of experience in custom tailoring creating perfect fitting custom business suits, custom shirts, custom clothing in BKK
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-1-of-4">
+                <div class="services-box">
+                    <i class="services-box__icon fas fa-tshirt"></i>
+                    <h3 class="heading-tertiary mb-4">Free <br> Styling</h3>
+                    <p class="services-box__text">
+                        Define your style with an style expert. Customize your look and consult with professinals. Get a perfect <b>Custom Suit at our shops.</b>
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-1-of-4">
+                <div class="services-box">
+                    <i class="services-box__icon fad fa-money-check-alt"></i>
+                    <h3 class="heading-tertiary mb-4">Affordable Pricing</h3>
+                    <p class="services-box__text">
+                        Premium <b>custom clothing in Yangon</b> at Affordable Prices. Custom Suits, Custom Shirts & More services.
+                    </p>
                 </div>
             </div>
         </div>
