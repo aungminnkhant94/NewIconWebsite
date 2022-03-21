7c06d07c91ac902659f07feab16e78f2a67d3d3c
aungminnkhant94 <aungminnkhant94@gmail.com>
2022-03-22 00:47:56 +0630

    review section

--- src/components/ReviewSection.vue
+++ src/components/ReviewSection.vue
@@ -1,9 +1,29 @@
 <template>
-    <section class="section-reviews">
+    <section class="section-reviews ms-auto">
         <div class="u-center-text u-margin-bottom-big">
             <h2 class="heading-secondary">
-                We make people genuinely look smart
+                We promote our beloved clients' business
             </h2>
+        </div>
+
+        <div class="row">
+            <div class="reviews ms-auto">
+                <figure class="reviews__shape">
+                    <img src="../assets/img/nat-8.jpg" alt="" class="reviews__img">
+                </figure>
+                <div class="reviews__text">
+                    <h3 class="heading-tertiary u-margin-bottom-small u-center-text"> သူဌေးစာ </h3>
+                    <br>
+                    <p class="reviews__paragraph">
+                        Shal Yaung က service တွေက ကျွန်တော့်အတွက်တော့ တော်တော်အဆင်ပြေတယ်
+                        လူငယ်တွေချည်း ဦးစီးတာ/လုပ်ကိုင်တာဆိုတော့ လုပ်ချင်တာဖြစ်ချင်တာတွေ တိုင်ပင်လို့ကောင်းတယ် ခေတ်အမြင်ရှိတယ် သဘောကျတာတစ်ခုက တာဝန်ယူပေးတဲ့လုပ်ငန်း ကို ထဲထဲဝင်ဝင်လေ့လာပြီးတော့ လိုအပ်တာတွေအကြံပေးတယ် ဖြည့်ဆည်းပေးတယ် Content အချိတ်အဆက်တွေလည်း မိမိုက်တယ်ပေါ့ဗျာ design ကလည်း ဆွဲဆောင်မှုရှိတယ်
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="u-center-text">
+            <a href="#" class="btn btn-secondary btn-review">Read all reviews &rarr;</a>
         </div>
     </section>
 </template>
