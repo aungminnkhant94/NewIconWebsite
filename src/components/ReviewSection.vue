5106e7e7bc130ac5232f421c2bf9f969e415a378
aungminnkhant94 <aungminnkhant94@gmail.com>
2022-03-22 23:20:40 +0630

    review section finished

--- src/components/ReviewSection.vue
+++ src/components/ReviewSection.vue
@@ -11,12 +11,41 @@
                 <figure class="reviews__shape">
                     <img src="../assets/img/nat-8.jpg" alt="" class="reviews__img">
                 </figure>
+                <h3 class="heading-review u-margin-bottom-small u-center-text"> သူဌေးစာ </h3>
                 <div class="reviews__text">
-                    <h3 class="heading-tertiary u-margin-bottom-small u-center-text"> သူဌေးစာ </h3>
                     <br>
                     <p class="reviews__paragraph">
-                        Shal Yaung က service တွေက ကျွန်တော့်အတွက်တော့ တော်တော်အဆင်ပြေတယ်
-                        လူငယ်တွေချည်း ဦးစီးတာ/လုပ်ကိုင်တာဆိုတော့ လုပ်ချင်တာဖြစ်ချင်တာတွေ တိုင်ပင်လို့ကောင်းတယ် ခေတ်အမြင်ရှိတယ် သဘောကျတာတစ်ခုက တာဝန်ယူပေးတဲ့လုပ်ငန်း ကို ထဲထဲဝင်ဝင်လေ့လာပြီးတော့ လိုအပ်တာတွေအကြံပေးတယ် ဖြည့်ဆည်းပေးတယ် Content အချိတ်အဆက်တွေလည်း မိမိုက်တယ်ပေါ့ဗျာ design ကလည်း ဆွဲဆောင်မှုရှိတယ်
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veritatis in sequi repellat minima mollitia natus saepe facere? Magnam aliquam atque nemo nobis delectus cum possimus maxime aspernatur veritatis esse?
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="reviews ms-auto">
+                <figure class="reviews__shape">
+                    <img src="../assets/img/nat-8.jpg" alt="" class="reviews__img">
+                </figure>
+                <h3 class="heading-review u-margin-bottom-small u-center-text"> သူဌေးစာ </h3>
+                <div class="reviews__text">
+                    <br>
+                    <p class="reviews__paragraph">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veritatis in sequi repellat minima mollitia natus saepe facere? Magnam aliquam atque nemo nobis delectus cum possimus maxime aspernatur veritatis esse?
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="reviews ms-auto">
+                <figure class="reviews__shape">
+                    <img src="../assets/img/nat-8.jpg" alt="" class="reviews__img">
+                </figure>
+                <h3 class="heading-review u-margin-bottom-small u-center-text"> သူဌေးစာ </h3>
+                <div class="reviews__text">
+                    <br>
+                    <p class="reviews__paragraph">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veritatis in sequi repellat minima mollitia natus saepe facere? Magnam aliquam atque nemo nobis delectus cum possimus maxime aspernatur veritatis esse?
                     </p>
                 </div>
             </div>
