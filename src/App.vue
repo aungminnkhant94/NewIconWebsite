34782ee9dba94e73f8bfc6a018c16a6e97a672f1
aungminnkhant94 <aungminnkhant94@gmail.com>
2023-04-08 23:22:46 +0630

    update

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,10 @@
                 <li class="nav-item">
                   <router-link to="/" class="nav-link fs-5" aria-current="page">Home</router-link>
                 </li>
-
-                <li class="nav-item dropdown">
+                <li class="nav-item">
+                  <a href="/#services" class="nav-link fs-5" aria-current="page" >Services</a>
+                </li>
+                <!-- <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle fs-5" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Collections
                   </a>
@@ -30,7 +32,6 @@
                     <li><router-link to="/accessories" class="dropdown-item">Accessories</router-link></li>
                   </ul>
                 </li>
-
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle fs-5" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Fabrics
@@ -40,16 +41,10 @@
                     <li><a class="dropdown-item" href="#">Another action</a></li>
                     <li><a class="dropdown-item" href="#">Something else here</a></li>
                   </ul>
-                </li>
-
+                </li> -->
                 <router-link to="/" class="navbar-brand d-none d-lg-block">
                  <img src="./assets/img/logo.jpg" alt="logo" width="45" height="42" class="d-inline-block align-text-center"> New Icon
                 </router-link>
-
-                <li class="nav-item">
-                  <a href="/#services" class="nav-link fs-5" aria-current="page" >Services</a>
-                </li>
-
                 <li class="nav-item">
                   <a class="nav-link fs-5" aria-current="page" href="/#reviews">Reviews</a>
                 </li>
@@ -70,7 +65,8 @@
     <div class="row">
       <div class="col-1-of-3">
         <h3 class="heading-footer u-center-text">
-          Our Story
+          <router-link to="/our-story" class="our__story">Our Story</router-link>
+          <!-- <a href="" class="our__story">Our Story</a> -->
         </h3>
         <p class="footer__paragraph">
           At New Icon, we create suits that are more than your expectations. From choosing the fabric to selecting the suit type, we are helping you to find and get a perfect-fitted suit for your desire occasion.
--- src/components/ReviewSection.vue
+++ src/components/ReviewSection.vue
@@ -11,7 +11,7 @@
                 <figure class="reviews__shape">
                     <img src="../assets/img/nat-8.jpg" alt="" class="reviews__img">
                 </figure>
-                <h3 class="heading-review u-margin-bottom-small u-center-text"> John Doe </h3>
+                <h3 class="heading-review u-margin-bottom-small u-center-text"> L Zi </h3>
                 <div class="reviews__stars">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -21,7 +21,7 @@
                 </div>
                 <div class="reviews__text">
                     <p class="reviews__paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veritatis in sequi repellat minima mollitia natus saepe facere? Magnam aliquam atque nemo nobis delectus cum possimus maxime aspernatur veritatis esse?
+                        Reliable service with friendly and collaborative communication. Should try once if you're looking for a quality end-product with reasonable prices. Highly Recommended.
                     </p>
                 </div>
             </div>
@@ -32,7 +32,7 @@
                 <figure class="reviews__shape">
                     <img src="../assets/img/nat-8.jpg" alt="" class="reviews__img">
                 </figure>
-                <h3 class="heading-review u-margin-bottom-small u-center-text"> John Doe </h3>
+                <h3 class="heading-review u-margin-bottom-small u-center-text"> Jonathan Swan Htet </h3>
                 <div class="reviews__stars">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -42,7 +42,7 @@
                 </div>
                 <div class="reviews__text">
                     <p class="reviews__paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veritatis in sequi repellat minima mollitia natus saepe facere? Magnam aliquam atque nemo nobis delectus cum possimus maxime aspernatur veritatis esse?
+                        Good quality, good tailor and they measured make surely for specific things for the suit. Their shop have lots of fabrics and designs. The tailor will make you what you want for your design. Not only for men but also for girls. I recommend New Icon shop is the good, fail price, make in right time and good service.
                     </p>
                 </div>
             </div>
@@ -53,8 +53,9 @@
                 <figure class="reviews__shape">
                     <img src="../assets/img/nat-8.jpg" alt="" class="reviews__img">
                 </figure>
-                <h3 class="heading-review u-margin-bottom-small u-center-text"> John Doe </h3>
+                <h3 class="heading-review u-margin-bottom-small u-center-text"> Thint Myat Soe</h3>
                 <div class="reviews__stars">
+                    <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -62,14 +63,14 @@
                 </div>
                 <div class="reviews__text">
                     <p class="reviews__paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veritatis in sequi repellat minima mollitia natus saepe facere? Magnam aliquam atque nemo nobis delectus cum possimus maxime aspernatur veritatis esse?
+                        A big thanks to New Icon Team for making me a wonderful suit: the jacket and pants are fantastic, beautifully done, fit perfectly, with incredible fabric and the suit looks amazing, 10 out of 10 for customer service, quality and price, I believe every gentlemen should try and experiecnce at least once at New Icon Custom Tailor!!!
                     </p>
                 </div>
             </div>
         </div>
 
         <div class="u-center-text">
-            <a href="#" class="btn btn-secondary btn-review">Read all reviews &rarr;</a>
+            <a href="#" class="reviews__button btn btn-secondary btn-review">Read all reviews &rarr;</a>
         </div>
     </section>
 </template>
